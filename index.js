15d2c4cd5b6eb378c1344e11459bf1e12ab15155
SachinRV18 <sachinrv1999@example.com>
2024-09-20 16:06:14 +0530

    part2 commit

--- index.js
+++ index.js
@@ -8,3 +8,36 @@
 // console.log([1,2,3,4]) //array
 
 // console.log({a:1,b:2,c:3}) //dict
+
+//variables
+
+// var name = "sachin";
+// // console.log(name)
+// name = "gowtham";
+// console.log(name) //gowtham is the output
+
+// var greeting;  // conditionally rendering some data
+// greeting = "hello"
+// console.log(greeting) //undefined is the output
+
+// var $name = "sachin";  // $ used more in Jquery
+// console.log($name);
+
+// let $name = "sachin";  // $ used more in Jquery
+// console.log($name);
+
+// const name = "sachin";
+// console.log(name)
+// name = "gowtham";
+// console.log(name) // error , because we can't reassign a values
+
+// const person = {
+//     name : "sachin",
+// };
+// person.name = "Ganesh"
+// console.log(person)
+// Note: we cannot change the value of the constant variable, but we can change the "value of the object" of constant variable
+
+// const numbers = [1,2,3,4,5,6]
+// numbers.push(7)
+// console.log(numbers)
