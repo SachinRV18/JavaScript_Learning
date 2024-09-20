d73b2822f12aebdbff0bd9d76f1180944ab5aa90
SachinRV18 <sachinrv1999@example.com>
2024-09-20 17:58:50 +0530

    part4 commit

--- index.js
+++ index.js
@@ -83,3 +83,102 @@
 // const today = new Date()
 // console.log(today)
 // console.log(typeof today)
+
+// let val;
+
+// // Number to string
+
+// val = 533;
+
+// val= String(val)
+// console.log(val)
+// console.log(typeof val)
+// console.log(val.length)
+
+// let val;
+
+// // Boolean to string
+
+// val = true;
+
+// val= String(val)
+// console.log(val)
+// console.log(typeof val)
+// console.log(val.length)
+
+// let val;
+
+// // Boolean to string
+
+// val = new Date();
+
+// val= String(val)
+// console.log(val)
+// console.log(typeof val)
+// console.log(val.length)
+
+// let val;
+// // Array to string
+// val = [1,2,3,4,5];  // why 9 because it counts a spaces also
+// val= String(val)
+// console.log(val)
+// console.log(typeof val)
+// console.log(val.length)
+
+// the above we can also done by using toString() method
+
+// let val;
+// // toString function
+// val = 5;
+// val= val.toString()
+// console.log(val)
+// console.log(typeof val)
+// console.log(val.length)
+
+// // string to a number
+// let val;
+// val = 5;
+// console.log(val)
+// console.log(typeof val)
+// console.log(val.toFixed(2))
+
+// let val;
+// val = "5";
+// val = "5";
+// val= Number(val)
+// console.log(val)
+// console.log(typeof val)
+// console.log(val.toFixed(2))
+
+// // boolean value
+// let val;
+// val1 = Number(true)
+// val2 = Number(false)
+// val3 = Number(null)
+
+// console.log(val1)
+// console.log(val2)
+// console.log(val3)
+// console.log(typeof val1)
+// console.log(typeof val2)
+// console.log(typeof val3)
+// console.log(val1.toFixed(2))
+// console.log(val2.toFixed(2))
+// console.log(val3.toFixed(2)) // null is always 0 
+
+// // using parseInt
+// let val;
+// val = "5";
+// val= parseInt(val)
+// console.log(val)
+// console.log(typeof val)
+// console.log(val.toFixed(2))
+
+
+// type cohesion
+const val1 = "5";
+const val2 = 6;
+const sum = val1 + val2
+
+console.log(sum)
+console.log(typeof sum)  //  if frst letter is string means it will convert next one also an string and concatinate its value
