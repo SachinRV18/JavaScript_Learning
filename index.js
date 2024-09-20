2b4282c8fad389d3376ef4b0d0a743383eb3cba0
SachinRV18 <sachinrv1999@example.com>
2024-09-20 15:37:13 +0530

    part1 commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// alert("Hello Sachin!!")
+// console.log(123)
+// console.log("sachin")
+
+// var my_name = "sachin";
+// console.log(my_name)
+
+// console.log([1,2,3,4]) //array
+
+// console.log({a:1,b:2,c:3}) //dict
