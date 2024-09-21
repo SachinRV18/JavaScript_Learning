d18a9c2ea17bc111802e37120c1c1eab07310fa1
SachinRV18 <sachinrv1999@example.com>
2024-09-21 15:34:38 +0530

    part7 commit

--- Practice_codes/app.js
+++ Practice_codes/app.js
@@ -1,17 +1,65 @@
-// Numbers and Math Object
-let val;
-val = Math.PI;
-val = Math.round(3.2)
-val = Math.ceil(3.1)  //above value
-val = Math.floor(3.1)  //lower/previous value
-val = Math.sqrt(4)
-val = Math.abs(-5)   // Absolute value.
-val = Math.pow(5, 2)   // power value.
-val = Math.min(2,3,5,67,887)  // Minimum
-val = Math.max(2,3,5,67,887)  // Maximum
-val = Math.random()  // Print the value within 0 to 1
-val = Math.random() * 20 // Print the value within 0 to 20
-val = Math.random() * 20 + 1// Print the value within 1 to 20
-val = Math.floor(Math.random() * 20 + 1)// Print the value within 1 to 20 (only whole number. Not Decimal values)
+// // arrays and methods
 
-console.log(val)
+// const number = [1,2,3,45,6,7,8,9,32];
+// const number2 = new Array(1,2,3,45,6,7,8,9,32);  // another way of creating an array
+// const fruits = new Array("apple", "bananana", "orange");  // astoring strings in array
+// // const mixed = new Array("apple", "bananana", " orange", 1, 2,3,4,5, true, undefined, null);  // storing multiple types of data's in array
+// // // console.log(number2)
+// // // console.log(fruits)
+// // // console.log(mixed)
+
+// let val;
+// // val = number.length; // array length
+
+// // val = Array.isArray(number); // to check its an array or not
+
+// // val = number[3] // to get a single value using indexing
+
+// // // insert or replace
+// // number[1] = 100;
+
+// // // find the index value
+// // val = number.indexOf(45);
+
+// // //Mutating the array
+// // //add a number to the end of an array
+// // number.push(2590);
+
+// // // add to the front
+// // number.unshift(120);
+
+// // //remove the last number from the array.
+// // number.pop(2590);
+
+// // // remove the frst number from the array.
+// // number.shift(120)
+
+// // console.log(number);
+
+// // //splicing
+// // number.splice(1,1)  // remove an element from pointing the index
+// // console.log(number);
+
+// // reverse the number
+// // number.reverse();
+
+// // concat the numbers
+// // val = number.concat(number2);
+
+// // sort the array
+// // val = fruits.sort();
+
+// // // sort the numbers
+// // val = number.sort()  // it will sort based on the first digit.
+
+// // // sort ascending order
+// // val = number.sort(function(x, y) {
+// //     return x-y;
+// // });
+
+// // sort decending order
+// val = number.sort(function(x, y) {
+//     return y - x;
+// });
+
+// console.log(val)
--- Practice_codes/index.js
+++ Practice_codes/index.js
@@ -248,3 +248,21 @@
 // val = sent.includes("Books")
 
 // console.log(val)
+
+// Numbers and Math Object
+// let val;
+// val = Math.PI;
+// val = Math.round(3.2)
+// val = Math.ceil(3.1)  //above value
+// val = Math.floor(3.1)  //lower/previous value
+// val = Math.sqrt(4)
+// val = Math.abs(-5)   // Absolute value.
+// val = Math.pow(5, 2)   // power value.
+// val = Math.min(2,3,5,67,887)  // Minimum
+// val = Math.max(2,3,5,67,887)  // Maximum
+// val = Math.random()  // Print the value within 0 to 1
+// val = Math.random() * 20 // Print the value within 0 to 20
+// val = Math.random() * 20 + 1// Print the value within 1 to 20
+// val = Math.floor(Math.random() * 20 + 1)// Print the value within 1 to 20 (only whole number. Not Decimal values)
+
+// console.log(val)
